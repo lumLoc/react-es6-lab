f4df94b234e6172f80712cc2ebc12ce2786a987c
Jarren S. Gatdula <luminance.10000@gmail.com>
2026-01-22 02:56:37 -0800

    Add verify users

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -6,6 +7,9 @@ import './App.css'
 // import components
 import UserList from './UserList.jsx'
 import Counter from './Counter.jsx'
+import UserAPI from './UserAPI.jsx'
+
+const isLoggedIn = true;
 
 function App() {
   const [count, setCount] = useState(0)
@@ -31,6 +35,10 @@ function App() {
         </a>
       </div>
 
+      <div>
+        {isLoggedIn ? <p>Welcome back!</p> : <p>Please log in</p>}
+      </div>
+
       <h1>Vite + React</h1>
 
       <div className="card">
@@ -44,13 +52,18 @@ function App() {
 
         <hr />
 
-        <h2>User List</h2>
+        <h2>User List (Local)</h2>
         <UserList users={users} />
 
         <hr />
 
         <h2>Counter Component</h2>
         <Counter />
+
+        <hr />
+
+        <h2>Users from API</h2>
+        <UserAPI />
       </div>
 
       <p className="read-the-docs">
